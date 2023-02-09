81a2fa8afa0c150af8c5e92819c99d8afd7f8340
Oscar Alva <oscaraldrin23@gmail.com>
2023-02-09 23:14:20 +0100

    sidebar done

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
 import {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
-import {Paper, IconButton} from '@mui/material';
-import {SearchIcon} from '@mui/icons-material';
+import {Paper, IconButton, Icon} from '@mui/material';
+import {Search} from '@mui/icons-material';
 
 const SearchBar = () => {
   return (
     <Paper
-        
+        component="form"
+        onSubmit={()=>{}}
+        sx={{
+            borderRadius: 20,
+            border: '1px solid #e3e3e3',
+            pl: 2,
+            boxShadow: 'none',
+            mr: {sm: 5}
+        }}
     >
-      Soy el search bar 
+        <input
+            className='search-bar'
+            placeholder='Search...'
+            value=""
+            onChange={()=>{}}
+        />
+        <IconButton type="submit" sx={{
+            p:'10px', color:'red'
+        }}>
+            <Search/>
+        </IconButton>
     </Paper>
   )
 }
